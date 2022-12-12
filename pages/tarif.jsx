b0f5fedcc7b7477da796dc5837986fef89553b05
Nimp <nimpweb@yandex.ru>
2022-12-13 00:27:45 +0500

    add 5

--- pages/tarif.jsx
+++ pages/tarif.jsx
@@ -8,7 +8,7 @@ export default function Limit() {
   >
     <Container>
       <h1 className="font-bold text-xl text-center mb-5">Обслуживание банковских карт на интернет-сайте Банка в целях осуществления перевода денежных средств в рамках Системы «Город»</h1>
-      <p>Плата не взимается, кроме некоторых услуг указанных в документе <a target="_blank" className="font-semibold text-slate-800 hover:text-slate-400 ease duration-200" href="https://orbank.ru/retail/retail-tariffs.php">Тарифы для физических лиц</a>. Вы будете ознакомлены с комиссией до совершения платежа.</p>
+      <p>Плата не взимается, кроме некоторых услуг указанных в документе <a target="_blank" rel="noreferrer" className="font-semibold text-slate-800 hover:text-slate-400 ease duration-200" href="https://orbank.ru/retail/retail-tariffs.php">Тарифы для физических лиц</a>. Вы будете ознакомлены с комиссией до совершения платежа.</p>
     </Container>
    </Layout>
 
