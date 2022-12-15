0e4cb03a87d5515d943fa929f2a7bb39be8c23f2
nimp <nimpweb@yandex.ru>
2022-12-15 16:38:13 +0500

    at work 11

--- pages/bill.jsx
+++ pages/bill.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import {Layout, Container} from '../components'
 import { GiPayMoney }  from 'react-icons/gi'
 import { TiSortNumerically }  from 'react-icons/ti'
-import {Modal} from 'next-modal'
+import { Modal } from 'next-modal'
+import { CiSaveDown2 }  from 'react-icons/ci'
+import { GrFormClose } from 'react-icons/gr'
 
 const bill = () => {
 
@@ -50,7 +52,12 @@ const bill = () => {
                   173 x 3,46 = <strong>₽ 685.08</strong>
                   </td>
                   <td className="py-4 px-6 text-center flex justify-end">
-                    <button className="bg-slate-800 py-1 px-3 rounded-xl text-white hover:bg-slate-500 cursor-pointer ease-linear duration-200 active:translate-y-1 flex items-center gap-1"><TiSortNumerically /> Внести показания</button>
+                    <button 
+                      className="bg-slate-800 py-1 px-3 rounded-xl text-white hover:bg-slate-500 cursor-pointer ease-linear duration-200 active:translate-y-1 flex items-center gap-1"
+                      onClick={() =>setShowModal(true)}
+                    >
+                      <TiSortNumerically /> Внести показания
+                    </button>
                   </td>
               </tr>
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
@@ -174,6 +181,26 @@ const bill = () => {
           </table>
         </div>
 
+
+      <Modal toggle={showModal} setToggle={() => setShowModal(prev => !prev)}>
+        <Modal.Header>Электроэнергия (<span className="text-sm">Прибор учета: <span className="font-semibold">Меркурий-201</span> / <span className="font-semibold">778945465465</span></span>)</Modal.Header>
+        <Modal.Body>
+          
+          <div className="rounded-xl border border-slate-500 m-auto p-3 mt-1">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga esse quo debitis. Labore vero autem atque blanditiis repellat qui dolor. Esse iusto facere veniam temporibus, consequuntur quidem minus culpa vitae.
+            Alias maxime corporis, tenetur dolores necessitatibus in! Quasi ipsa hic error asperiores minima tempora provident, inventore possimus. Ipsa, quasi laboriosam? Eligendi voluptatum voluptates minus sequi quaerat placeat, officia enim dolorem.
+          </div>          
+        </Modal.Body>
+        <Modal.Footer className="flex gap-5">
+          <button className="border border-transparent bg-slate-800 text-white hover:bg-slate-500 ease duration-200 translate-y-1 py-1 px-5 rounded-xl flex items-center gap-1"><CiSaveDown2 /> Сохранить</button>
+          <button 
+            className="border border-slate-800 text-slate-800 bg-white hover:bg-slate-800 hover:text-white ease duration-200 translate-y-1 py-1 px-5 rounded-xl flex items-center gap-1"
+            onClick={() => setShowModal(false)}
+          >
+            <GrFormClose /> Закрыть
+          </button>
+        </Modal.Footer>
+      </Modal>
 
       </Container>
     </Layout>
